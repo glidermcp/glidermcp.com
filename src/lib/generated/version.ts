78f3b754c61fa006ab28fa82b4215a33d7da13c9
Bogdan Novosad <bogdan@sacrorum.com>
2026-02-02 02:40:01 +0100

    feat: update gliderVersion to 4.1.0 with new fetchedAt timestamp

--- src/lib/generated/version.ts
+++ src/lib/generated/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated. Do not edit.
 export const gliderVersion = {
-	version: '4.0.0',
-	fetchedAt: '2026-01-23T12:24:04.314Z'
+	version: '4.1.0',
+	fetchedAt: '2026-02-02T00:37:16.648Z'
 };
